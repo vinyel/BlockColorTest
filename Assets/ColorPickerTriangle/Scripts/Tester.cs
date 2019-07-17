c7f98e3689817a9478fa6457e52ece037306012b
U-VAIO\apple <vinyel422@gmail.com>
2019-07-18 04:45:50 +0900

    implement moji test

---
+++ Assets/ColorPickerTriangle/Scripts/Tester.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using System.Linq;
+
+public class Tester : MonoBehaviour
+{
+    [Range(1,6)]
+    public int NumOfTrial = 1; //1=12, 2=24
+
+    List<string> pitch = new List<string> { "ド", "ド＃", "レ", "レ＃", "ミ", "ファ", "ファ＃", "ソ", "ソ＃", "ラ", "ラ＃", "シ"};
+    List<string> totalPitch = new List<string>();
+
+    public GameObject ColorPickedPrefab;
+    private GameObject go;
+    private ColorPickerTriangle CP;
+    StreamWriter sw;
+
+    private int num = 0;
+    private bool isTest = true;
+
+    DateTime dtNow;
+    string dtName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for ( int i = 0; i < NumOfTrial; i++) {
+            foreach (string a in pitch) {
+                totalPitch.Add(a);
+            }
+        }
+        totalPitch = totalPitch.OrderBy(a => Guid.NewGuid()).ToList();
+        Debug.Log(totalPitch[num]);
+
+        dtNow = DateTime.Now;
+        dtName = dtNow.Month.ToString() + dtNow.Day.ToString() + dtNow.Hour.ToString() + dtNow.Minute.ToString();
+        sw = new StreamWriter("../TestData"+ dtName + ".txt", false);
+
+        go = ColorPickedPrefab;
+        CP = go.GetComponent<ColorPickerTriangle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ToNextTest() {
+        if (num >= (NumOfTrial * 12) - 1 && isTest) {
+            sw.WriteLine(totalPitch[num] + ":" + CP.TheColor.r + ", " + CP.TheColor.g + ", " + CP.TheColor.b);
+            sw.Flush();// StreamWriterのバッファに書き出し残しがないか確認
+            sw.Close();// ファイルを閉じる
+            isTest = false;
+            Debug.Log("終了です");
+        }
+        //カラーピッカーの色を取得
+        //ファイル書込み
+        if (isTest) {
+            sw.WriteLine(totalPitch[num] + ":" + CP.TheColor.r + ", " + CP.TheColor.g + ", " + CP.TheColor.b); //音階：[R, G, B]
+            num++;
+            Debug.Log(totalPitch[num]);
+        }
+        //カラーピッカーの色を白にリセット
+        CP.TheColor = Color.white;
+    }
+}
--- Assets/Scripts/AttachColor.cs
+++ Assets/Scripts/AttachColor.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class AttachColor : MonoBehaviour
 {
     public GameObject ColorPickedPrefab;
+    private Material mat;
+    private bool isPaint = false;
+    private ColorPickerTriangle CP;
+    private GameObject go;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mat = GetComponent<MeshRenderer>().material;
+        go = ColorPickedPrefab;
+        CP = go.GetComponent<ColorPickerTriangle>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //CP.SetNewColor(mat.color);
+        mat.color = CP.TheColor;   
     }
+
 }
